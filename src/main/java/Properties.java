001374e4d62fbe10f5fe1306ba047b28bd58f489
danuri <gksdudrb922@naver.com>
2021-07-01 23:39:18 +0900

    Fixed Untracked files

--- src/main/java/Properties.java
+++
@@ -1,4 +0,0 @@
-public class Properties {
-    public static final String PERSONAL_ACCESS_TOKEN = "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";
-    public static final String TARGET_REPOSITORY = "whiteship/live-study";
-}
